69186bfb0f48112f6fc52ed1bfe7e302da20f9b0
RuntyConjurer <169057857+RuntyConjurer@users.noreply.github.com>
2024-10-08 18:48:08 -0400

    Add files via upload

---
+++ Evidencias2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Evidencias2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            Console.Write("Type in your first name");
+            string MyFirstName;
+            MyFirstName = Console.ReadLine();
+
+            string MyLastName;
+            Console.Write("Type your last name");
+            MyLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + MyFirstName + MyLastName);
+
+                
+              
+        }
+    }
+}
