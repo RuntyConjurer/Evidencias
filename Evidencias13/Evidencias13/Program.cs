e2f9cc093fd283681d880edaf5abc6512a66b297
RuntyConjurer <169057857+RuntyConjurer@users.noreply.github.com>
2024-10-19 17:38:49 -0400

    Add files via upload

---
+++ Evidencias13/Evidencias13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.Generic;
+
+namespace lol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Primer numero:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Segundo numero:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+           
+            double suma = num1 + num2;
+                       
+            Console.WriteLine("sumados: " + suma);
+        }
+    }
+
+}
