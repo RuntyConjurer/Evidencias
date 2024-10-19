fcac90f80f472879bf4150c632ea335a374f5e4c
RuntyConjurer <169057857+RuntyConjurer@users.noreply.github.com>
2024-10-19 17:51:21 -0400

    Add files via upload

---
+++ Evidencias15/c####/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+while (true)
+{
+    Console.Write("Insete las existencias"); int cantidad;
+
+    bool esNumeroValido = int.TryParse(Console.ReadLine(), out cantidad);
+
+    if (!esNumeroValido)
+    {
+        Console.WriteLine("Ingrese un numero valido");
+        continue;
+    }
+
+    
+    if (cantidad >= 40 && cantidad >= 30)
+    {
+        Console.WriteLine("Sistema abastecido");
+    }
+
+    else if (cantidad >= 20 && cantidad <= 30)
+
+    {
+        Console.WriteLine("Sistema con existencias");
+
+    }
+
+    else if (cantidad <=10  && cantidad > 5)
+    {
+        Console.WriteLine("Sistema con problemas de existencias");
+    }
+
+    else if (cantidad <= 5)
+    {
+        Console.WriteLine("Abastece el sistema");
+
+
+    }
+
+}
+
+
+
+
+
+
+
